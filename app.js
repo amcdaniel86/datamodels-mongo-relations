13a18b4ba7025b740dbfc0655678add7a510e298
Alex McDaniel <amcdaniel186@gmail.com>
2018-11-12 14:43:49 -0500

    update1

---
+++ app.js
@@ -0,0 +1,60 @@
+// Twitter
+
+// - users
+      username
+      password
+      followers[user ids]
+      tweets [tweet ids]
+      likes [tweet ids]
+// - tweets
+      content
+      date
+      author ID
+
+// - followers (within users)
+// - favorites (within tweets)
+
+// Airbnb
+
+  // - users
+      { username
+      password
+      homes: [ids]
+      favorites [home ids]
+      rental history [booking ids]
+      reviews [booking ids]
+      }
+  // - homes
+      {
+          name: string
+            address: {
+              number: string
+              street: string
+              suite: string
+              city: string
+              zip: number
+            }
+
+          owner: ID (user ID)
+      }
+      price 
+      reviews
+      location
+
+  // - bookings
+// - when user reserves a home for time. 
+//         has user and home. example of many to many.
+
+//         relationships
+//         {
+//           //  a join table below, joins two different collections together.
+//           user1ID: id
+//           user2ID: id
+//         }
+          userId: id
+          homeId: id
+          startDate: date
+          endDate: date  
+          reviews[id] (of reviews)
+
+  // - reviews
